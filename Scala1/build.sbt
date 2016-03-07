fc3da5cf8e0a5a80de9ff01453c1a8d431d4eced
dheeraj <rythmnite66@gmail.com>
2016-03-07 09:29:45 -0600

    Setup And Initial code

---
+++ Scala1/build.sbt
@@ -0,0 +1,18 @@
+lazy val root = (project in file(".")).
+  settings(
+    name := "Scala1",
+    version := "1.0",
+    scalaVersion := "2.11.7",
+    mainClass in Compile := Some("WordCount")
+  )
+
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.2.0" % "provided",
+  "org.apache.spark" %% "spark-streaming" % "1.2.0" % "provided",
+  "org.apache.spark" % "spark-streaming-twitter_2.10" % "1.2.0"
+)
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
---
+++ Scala1/src/main/scala/WordCount.scala
@@ -0,0 +1,22 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+
+/**
+ * Created by dheeraj on 3/4/16.
+ */
+object WordCount {
+  def main(args: Array[String]) = {
+    val output ="output/wordcount"
+    val sc = new SparkContext("local", "WordCount", new SparkConf())
+    try {
+      val input = sc.textFile("input.csv").map(line => line.toUpperCase())
+      val result = input.flatMap(line=>line.split(","))
+        .map(word=>(word,1))
+        .reduceByKey(_+_)
+      result.saveAsTextFile(output)
+      println("Word Count Problem")
+    } finally {
+      sc.stop()
+    }
+  }
+}
